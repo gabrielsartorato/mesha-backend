95e577727fa4e6a7098c73299a1bc82731618226
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2021-04-05 23:02:06 -0300

    add: celebrate libary

--- src/routes/Users/users.routes.ts
+++ src/routes/Users/users.routes.ts
@@ -1,32 +1,18 @@
 import { Router } from 'express';
 import { UserController } from '@controllers/Users/UsersController';
-// import { celebrate, Joi, Segments } from 'celebrate';
-// import ensureAuthenticate from '@middlewares/ensureAuthenticade';
-// import { SessionController } from '@controllers/SessionsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 export const usersRouter = Router();
 
 const usersController = new UserController();
-// const sessionController = new SessionController();
 
 usersRouter.post(
   '/',
-  // celebrate({
-  //   [Segments.BODY]: {
-  //     user_name: Joi.string().required(),
-  //     password: Joi.string().required(),
-  //   },
-  // }),
+  celebrate({
+    [Segments.BODY]: {
+      user_name: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
   usersController.create,
 );
-
-// usersRouter.patch(
-//   '/',
-//   celebrate({
-//     [Segments.BODY]: {
-//       password: Joi.string().required(),
-//     },
-//   }),
-//   ensureAuthenticate,
-//   sessionController.update,
-// );
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import AppError from '@errors/AppError';
 import { routes } from './routes';
@@ -15,6 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
