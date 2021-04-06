39da757ec9c3bfdda6b143ccbc6a87c6c2e17e79
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2021-04-05 22:12:26 -0300

    add: initial server ts configs

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,33 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+import AppError from '@errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('Server is running at port 3333');
+});
